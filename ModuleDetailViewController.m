eaa1c2e2eaa5ff7be72f4ef8336139debecc49dd
Raymond HS <raymondhs2007@hotmail.com>
2011-08-09 12:49:48 +0800

    Revamped detail view for iPad

--- ModuleDetailViewController.m
+++ ModuleDetailViewController.m
@@ -2,24 +2,19 @@
 //  ModuleDetailViewController.m
 //  NUS Mod
 //
-//  Created by Steven Zhang on 12/7/11.
+//  Created by Raymond Hendy on 8/9/11.
 //  Copyright 2011 NUS. All rights reserved.
 //
 
 #import "ModuleDetailViewController.h"
 
 @implementation ModuleDetailViewController
-@synthesize titleLabel;
-@synthesize timeLabel;
-@synthesize descriptionTextView;
-@synthesize delegate = _delegate;
-//@synthesize showButtons;
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (id)initWithStyle:(UITableViewStyle)style
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    self = [super initWithStyle:style];
     if (self) {
-        showButtons = YES;
+        // Custom initialization
     }
     return self;
 }
@@ -37,32 +32,39 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self setShowButtons:showButtons];
 
-    if (module) {
-        self.titleLabel.text = [NSString stringWithFormat:@"%@: %@", module.code, module.title];
-        self.timeLabel.text = [NSString stringWithFormat:@"%@: %@", @"Workload:", module.workload];
-        self.descriptionTextView.text = module.moduleDescription;
-        [self updateButtons];
-    }; // trigger UI update
-}
-
-
-- (void)viewWillDisappear:(BOOL)animated {
-    [self setModule:nil];
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)viewDidUnload
 {
-    [self setTitleLabel:nil];
-    [self setTimeLabel:nil];
-    [self setDescriptionTextView:nil];
-    enableButton = nil;
-    removeButton = nil;
     [super viewDidUnload];
-    
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -71,64 +73,87 @@
 	return YES;
 }
 
-- (void)setModule:(Module *)newModule {
-    if (module)
-        [module removeObserver:self forKeyPath:@"enabled"];
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
     
-    NSLog(@"setModule:");
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+    }
     
-    module = newModule;
-
-    self.titleLabel.text = [NSString stringWithFormat:@"%@: %@", module.code, module.title];
-    self.timeLabel.text = module.workload;
-    self.descriptionTextView.text = module.moduleDescription;
-    [self updateButtons];
-    [module addObserver:self forKeyPath:@"enabled" options:NSKeyValueObservingOptionNew context:NULL];
+    // Configure the cell...
     
+    return cell;
 }
 
-- (Module *)module {
-    return module;
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
 }
+*/
 
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
-    if ([keyPath isEqualToString:@"enabled"]) {
-        [self updateButtons];
-    }
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
 }
+*/
 
-- (void)updateButtons {
-    if ([module.enabled boolValue]) {
-        [enableButton setTitle:@"Hide" forState:UIControlStateNormal];
-        [enableButton setTitle:@"Hide" forState:UIControlStateHighlighted];
-    } else  {
-            [enableButton setTitle:@"Show" forState:UIControlStateNormal];
-            [enableButton setTitle:@"Show" forState:UIControlStateHighlighted];
-    }
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
 }
+*/
 
-- (IBAction)handleEnableButton:(id)sender {
-    if ([_delegate respondsToSelector:@selector(enableButtonTappedForModule:)]) {
-        [_delegate performSelector:@selector(enableButtonTappedForModule:) withObject:module];
-    }
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
 }
+*/
 
-- (IBAction)handleRemoveButton:(id)sender {
-    if ([_delegate respondsToSelector:@selector(removeButtonTappedForModule:)]) {
-        [_delegate performSelector:@selector(removeButtonTappedForModule:) withObject:module];
-    }
-}
+#pragma mark - Table view delegate
 
-- (BOOL)showButtons {
-    return showButtons;
-}
-
-- (void)setShowButtons:(BOOL)show {
-    showButtons = show;
-    enableButton.hidden = !show;
-    removeButton.hidden = !show;
-        
-
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     [detailViewController release];
+     */
 }
 
 @end
